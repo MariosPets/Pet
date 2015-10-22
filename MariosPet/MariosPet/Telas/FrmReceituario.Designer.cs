0a5cfd230cfd4fe44f28ba057bb18f40046d0a60
Plínio <pliniosp@hotmail.com>
2015-10-22 18:24:59 -0200

    tela receituario

--- MariosPet/MariosPet/Telas/FrmReceituario.Designer.cs
+++ MariosPet/MariosPet/Telas/FrmReceituario.Designer.cs
@@ -28,32 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtNomeVeterinario = new System.Windows.Forms.TextBox();
+            this.txtPrescricao = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnSalvar
             // 
-            this.button1.Location = new System.Drawing.Point(113, 104);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSalvar.BackgroundImage = global::MariosPet.Properties.Resources.botaoSalvarVerde;
+            this.btnSalvar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnSalvar.FlatAppearance.BorderSize = 0;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar.Location = new System.Drawing.Point(600, 15);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(175, 44);
+            this.btnSalvar.TabIndex = 0;
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // txtNomeVeterinario
+            // 
+            this.txtNomeVeterinario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.txtNomeVeterinario.Location = new System.Drawing.Point(192, 156);
+            this.txtNomeVeterinario.Name = "txtNomeVeterinario";
+            this.txtNomeVeterinario.Size = new System.Drawing.Size(260, 32);
+            this.txtNomeVeterinario.TabIndex = 1;
+            // 
+            // txtPrescricao
+            // 
+            this.txtPrescricao.Location = new System.Drawing.Point(21, 330);
+            this.txtPrescricao.Name = "txtPrescricao";
+            this.txtPrescricao.Size = new System.Drawing.Size(524, 482);
+            this.txtPrescricao.TabIndex = 2;
+            this.txtPrescricao.Text = "";
             // 
             // FrmReceituario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = global::MariosPet.Properties.Resources.receituario1038X574;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(993, 1045);
+            this.Controls.Add(this.txtPrescricao);
+            this.Controls.Add(this.txtNomeVeterinario);
+            this.Controls.Add(this.btnSalvar);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.Name = "FrmReceituario";
             this.Text = "FrmReceituario";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtNomeVeterinario;
+        private System.Windows.Forms.RichTextBox txtPrescricao;
     }
 }
--- MariosPet/MariosPet/Telas/FrmReceituario.cs
+++ MariosPet/MariosPet/Telas/FrmReceituario.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Reporting.WebForms;
+using System.IO;
+using System.Diagnostics;
 
 namespace MariosPet.Telas
 {
@@ -15,6 +12,41 @@ namespace MariosPet.Telas
         public FrmReceituario()
         {
             InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //local
+            ReportViewer report = new ReportViewer();
+            report.ProcessingMode = ProcessingMode.Local;
+
+            report.LocalReport.ReportEmbeddedResource = "MariosPet.Report1.rdlc";
+
+            //criar listas
+            List<ReportParameter> lista = new List<ReportParameter>();
+
+            lista.Add(new ReportParameter("NomeVeterinario", txtNomeVeterinario.Text));
+            lista.Add(new ReportParameter("Prescricao", txtPrescricao.Text));
+
+            report.LocalReport.SetParameters(lista);
+
+            Warning[] warnings;
+            string[] streamids;
+            string mimeType;
+            string encoding;
+            string extrension;
+
+            byte[] bytePdf = report.LocalReport.Render("PDF", null, out mimeType, out encoding ,out extrension, out streamids, out warnings);
+
+            //criar pdf
+            FileStream file = null;
+            string nomeArquivo = Path.GetTempPath() + "Receiturario" + DateTime.Now.ToString("dd_MM_yyyy-HH_mm_ss") + ".pdf";
+
+            //abrir pdf
+            file = new FileStream(nomeArquivo, FileMode.Create);
+            file.Write(bytePdf, 0, bytePdf.Length);
+            file.Close();
+            Process.Start(nomeArquivo);
         }
     }
 }
