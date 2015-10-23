1a918e163d7e8c7ce1a85fd9d7d33a535799fe52
JESSICA FERREIRA <jessica.ferreira7@fatec.sp.gov.br>
2015-10-23 07:46:30 -0200

    carterinha

--- MariosPet/MariosPet/Telas/FrmCateirinha.Designer.cs
+++ MariosPet/MariosPet/Telas/FrmCateirinha.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -45,14 +45,17 @@
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnSalvar
             // 
-            this.button1.Location = new System.Drawing.Point(345, 326);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSalvar.BackgroundImage = global::MariosPet.Properties.Resources.botaoSalvarVerde;
+            this.btnSalvar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnSalvar.FlatAppearance.BorderSize = 0;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar.Location = new System.Drawing.Point(291, 309);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(129, 40);
+            this.btnSalvar.TabIndex = 0;
+            this.btnSalvar.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
@@ -66,89 +69,100 @@
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(116, 29);
+            this.txtNome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.txtNome.Location = new System.Drawing.Point(213, 32);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.Size = new System.Drawing.Size(175, 20);
             this.txtNome.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F);
             this.label1.Location = new System.Drawing.Point(10, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.Size = new System.Drawing.Size(178, 23);
             this.label1.TabIndex = 3;
             this.label1.Text = "Nome do Animal:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(71, 72);
+            this.label2.Font = new System.Drawing.Font("Verdana", 14.25F);
+            this.label2.Location = new System.Drawing.Point(136, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.Size = new System.Drawing.Size(52, 23);
             this.label2.TabIndex = 5;
             this.label2.Text = "Cor:";
             // 
             // txtCor
             // 
-            this.txtCor.Location = new System.Drawing.Point(116, 69);
+            this.txtCor.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.txtCor.Location = new System.Drawing.Point(213, 72);
             this.txtCor.Name = "txtCor";
-            this.txtCor.Size = new System.Drawing.Size(100, 20);
+            this.txtCor.Size = new System.Drawing.Size(175, 20);
             this.txtCor.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(63, 112);
+            this.label3.Font = new System.Drawing.Font("Verdana", 14.25F);
+            this.label3.Location = new System.Drawing.Point(122, 112);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.Size = new System.Drawing.Size(66, 23);
             this.label3.TabIndex = 7;
             this.label3.Text = "Sexo:";
             // 
             // txtSexo
             // 
-            this.txtSexo.Location = new System.Drawing.Point(116, 109);
+            this.txtSexo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.txtSexo.Location = new System.Drawing.Point(213, 112);
             this.txtSexo.Name = "txtSexo";
-            this.txtSexo.Size = new System.Drawing.Size(100, 20);
+            this.txtSexo.Size = new System.Drawing.Size(175, 20);
             this.txtSexo.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(61, 148);
+            this.label4.Font = new System.Drawing.Font("Verdana", 14.25F);
+            this.label4.Location = new System.Drawing.Point(124, 145);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.Size = new System.Drawing.Size(64, 23);
             this.label4.TabIndex = 9;
             this.label4.Text = "Raça:";
             // 
             // txtRaca
             // 
-            this.txtRaca.Location = new System.Drawing.Point(116, 145);
+            this.txtRaca.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.txtRaca.Location = new System.Drawing.Point(213, 148);
             this.txtRaca.Name = "txtRaca";
-            this.txtRaca.Size = new System.Drawing.Size(100, 20);
+            this.txtRaca.Size = new System.Drawing.Size(175, 20);
             this.txtRaca.TabIndex = 8;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(34, 182);
+            this.label5.Font = new System.Drawing.Font("Verdana", 14.25F);
+            this.label5.Location = new System.Drawing.Point(57, 182);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.Size = new System.Drawing.Size(131, 23);
             this.label5.TabIndex = 11;
             this.label5.Text = "Proprietario:";
             // 
             // txtProprietario
             // 
-            this.txtProprietario.Location = new System.Drawing.Point(116, 182);
+            this.txtProprietario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.txtProprietario.Location = new System.Drawing.Point(213, 185);
             this.txtProprietario.Name = "txtProprietario";
-            this.txtProprietario.Size = new System.Drawing.Size(100, 20);
+            this.txtProprietario.Size = new System.Drawing.Size(175, 20);
             this.txtProprietario.TabIndex = 10;
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
             this.groupBox1.Controls.Add(this.txtCor);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnSalvar);
             this.groupBox1.Controls.Add(this.txtNome);
             this.groupBox1.Controls.Add(this.txtProprietario);
             this.groupBox1.Controls.Add(this.label1);
@@ -157,17 +171,18 @@
             this.groupBox1.Controls.Add(this.txtRaca);
             this.groupBox1.Controls.Add(this.txtSexo);
             this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
             this.groupBox1.Location = new System.Drawing.Point(563, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(426, 355);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
             // 
             // FrmCateirinha
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(1027, 408);
             this.Controls.Add(this.groupBox1);
@@ -183,7 +198,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSalvar;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.Label label1;
