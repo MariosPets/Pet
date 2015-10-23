6c1b1062e36e054c01b50198d1e14cecc0ee971e
Tsunemoto <cintia.tsunemoto@fatec.sp.gov.br>
2015-10-23 11:59:14 -0200

    CrudServ

--- MariosPet/MariosPet/Crud/CrudServico.cs
+++ MariosPet/MariosPet/Crud/CrudServico.cs
@@ -17,8 +17,7 @@ namespace MariosPet.Crud
             {
                 string sql = "insert into SERVICO (ID_SERVICO, DESCRICAO, PRECO) values(?,?,?)";
                 OdbcCommand command = new OdbcCommand(sql, conexao);
-
-                command.Parameters.AddWithValue("@ID_SERVICO", servico.id);
+                                
                 command.Parameters.AddWithValue("@DESCRICAO", servico.descricao);
                 command.Parameters.AddWithValue("@PRECO", servico.preco);
 
