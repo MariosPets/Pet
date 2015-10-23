7c04d006a47c763bedd69b69247dece83b8e7009
Tsunemoto <cintia.tsunemoto@fatec.sp.gov.br>
2015-10-23 17:30:40 -0200

    dd

--- MariosPet/MariosPet/Classes/FichaClinica.cs
+++ MariosPet/MariosPet/Classes/FichaClinica.cs
@@ -14,12 +14,12 @@ namespace MariosPet.Classes
         public int idAnimal { get; set; }
         public int idVeterinario { get; set; }
         public DateTime data { get; set; }
-        public string historicoClinico { get; set; }
         public string queixaPrincipal { get; set; }
+        public string historicoClinico { get; set; }        
         public string suspeita { get; set; }
-        public string prescricao { get; set; }
         public string sintomas { get; set; }
         public string examesComplementares { get; set; }
+        public string prescricao { get; set; }         
         public string observacao { get; set; }
 
         #endregion
