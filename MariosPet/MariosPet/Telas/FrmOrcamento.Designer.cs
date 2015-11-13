09072263a312dbccb3f6917d94eabfc228a4c04a
JESSICA FERREIRA <jessica.ferreira7@fatec.sp.gov.br>
2015-11-13 08:07:38 -0200

    orçamento

---
+++ MariosPet/MariosPet/Telas/FrmOrcamento.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace MariosPet.Telas
+{
+    partial class FrmOrcamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmOrcamento));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtUnidade = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblTelefone2 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.txtIdentificacao = new System.Windows.Forms.TextBox();
+            this.lblTelefone1 = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.btmLimpar = new System.Windows.Forms.Button();
+            this.btmVoltar = new System.Windows.Forms.Button();
+            this.btmSalvar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
+            this.groupBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox1.BackgroundImage")));
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.txtUnidade);
+            this.groupBox1.Controls.Add(this.txtTotal);
+            this.groupBox1.Controls.Add(this.lblTelefone);
+            this.groupBox1.Controls.Add(this.lblTelefone2);
+            this.groupBox1.Controls.Add(this.txtDescricao);
+            this.groupBox1.Controls.Add(this.txtQuantidade);
+            this.groupBox1.Controls.Add(this.txtIdentificacao);
+            this.groupBox1.Controls.Add(this.lblTelefone1);
+            this.groupBox1.Controls.Add(this.lblEmail);
+            this.groupBox1.Controls.Add(this.lblNome);
+            this.groupBox1.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.groupBox1.Location = new System.Drawing.Point(95, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(717, 314);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "   Orçamento ";
+            // 
+            // txtUnidade
+            // 
+            this.txtUnidade.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnidade.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.txtUnidade.Location = new System.Drawing.Point(156, 203);
+            this.txtUnidade.MaxLength = 50;
+            this.txtUnidade.Name = "txtUnidade";
+            this.txtUnidade.Size = new System.Drawing.Size(170, 27);
+            this.txtUnidade.TabIndex = 19;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.txtTotal.Location = new System.Drawing.Point(156, 252);
+            this.txtTotal.MaxLength = 15;
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(170, 27);
+            this.txtTotal.TabIndex = 7;
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone.Location = new System.Drawing.Point(6, 252);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(65, 23);
+            this.lblTelefone.TabIndex = 18;
+            this.lblTelefone.Text = "Total:";
+            // 
+            // lblTelefone2
+            // 
+            this.lblTelefone2.AutoSize = true;
+            this.lblTelefone2.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone2.Location = new System.Drawing.Point(6, 202);
+            this.lblTelefone2.Name = "lblTelefone2";
+            this.lblTelefone2.Size = new System.Drawing.Size(97, 23);
+            this.lblTelefone2.TabIndex = 16;
+            this.lblTelefone2.Text = "Unidade:";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescricao.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.txtDescricao.Location = new System.Drawing.Point(156, 158);
+            this.txtDescricao.MaxLength = 15;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(255, 27);
+            this.txtDescricao.TabIndex = 5;
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantidade.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.txtQuantidade.Location = new System.Drawing.Point(156, 116);
+            this.txtQuantidade.MaxLength = 50;
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(61, 27);
+            this.txtQuantidade.TabIndex = 4;
+            // 
+            // txtIdentificacao
+            // 
+            this.txtIdentificacao.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdentificacao.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(70)))), ((int)(((byte)(46)))));
+            this.txtIdentificacao.Location = new System.Drawing.Point(10, 74);
+            this.txtIdentificacao.MaxLength = 80;
+            this.txtIdentificacao.Name = "txtIdentificacao";
+            this.txtIdentificacao.Size = new System.Drawing.Size(487, 27);
+            this.txtIdentificacao.TabIndex = 0;
+            // 
+            // lblTelefone1
+            // 
+            this.lblTelefone1.AutoSize = true;
+            this.lblTelefone1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone1.Location = new System.Drawing.Point(6, 158);
+            this.lblTelefone1.Name = "lblTelefone1";
+            this.lblTelefone1.Size = new System.Drawing.Size(112, 23);
+            this.lblTelefone1.TabIndex = 5;
+            this.lblTelefone1.Text = "Descrição:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(6, 116);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(128, 23);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Quantidade:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(6, 48);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(405, 23);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Identificação do Veterinário/Funcionario:";
+            // 
+            // btmLimpar
+            // 
+            this.btmLimpar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btmLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
+            this.btmLimpar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btmLimpar.BackgroundImage")));
+            this.btmLimpar.FlatAppearance.BorderSize = 0;
+            this.btmLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btmLimpar.Location = new System.Drawing.Point(497, 374);
+            this.btmLimpar.Name = "btmLimpar";
+            this.btmLimpar.Size = new System.Drawing.Size(117, 44);
+            this.btmLimpar.TabIndex = 6;
+            this.btmLimpar.UseVisualStyleBackColor = false;
+            // 
+            // btmVoltar
+            // 
+            this.btmVoltar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btmVoltar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
+            this.btmVoltar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btmVoltar.BackgroundImage")));
+            this.btmVoltar.FlatAppearance.BorderSize = 0;
+            this.btmVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btmVoltar.Location = new System.Drawing.Point(299, 375);
+            this.btmVoltar.Name = "btmVoltar";
+            this.btmVoltar.Size = new System.Drawing.Size(116, 44);
+            this.btmVoltar.TabIndex = 5;
+            this.btmVoltar.UseVisualStyleBackColor = false;
+            // 
+            // btmSalvar
+            // 
+            this.btmSalvar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btmSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
+            this.btmSalvar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btmSalvar.BackgroundImage")));
+            this.btmSalvar.FlatAppearance.BorderSize = 0;
+            this.btmSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btmSalvar.Location = new System.Drawing.Point(695, 376);
+            this.btmSalvar.Name = "btmSalvar";
+            this.btmSalvar.Size = new System.Drawing.Size(117, 42);
+            this.btmSalvar.TabIndex = 7;
+            this.btmSalvar.UseVisualStyleBackColor = false;
+            // 
+            // FrmOrcamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(901, 445);
+            this.Controls.Add(this.btmLimpar);
+            this.Controls.Add(this.btmVoltar);
+            this.Controls.Add(this.btmSalvar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmOrcamento";
+            this.Text = "Orçamento";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtUnidade;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Label lblTelefone2;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.TextBox txtIdentificacao;
+        private System.Windows.Forms.Label lblTelefone1;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Button btmLimpar;
+        private System.Windows.Forms.Button btmVoltar;
+        private System.Windows.Forms.Button btmSalvar;
+    }
+}
