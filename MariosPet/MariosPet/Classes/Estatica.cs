8c47963cbd5f0ce499708687563b517261ff52b7
leonardofernando06 <leonardo.sales@fatec.sp.gov.br>
2015-11-13 08:52:27 -0200

    as

--- MariosPet/MariosPet/Classes/Estatica.cs
+++ MariosPet/MariosPet/Classes/Estatica.cs
@@ -3,9 +3,9 @@
     static class Estatica
     {
         public static int id;
-        public static Cliente cliente;
+        public static Cliente cliente = new Cliente();
         public static Funcionario funcionario = new Funcionario();
-        public static Animal animal;
+        public static Animal animal = new Animal();
         public static bool novo;
     }
 }
