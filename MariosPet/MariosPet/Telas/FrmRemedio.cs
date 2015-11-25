551fe00bbd6c8ea580df9865e7e7deb409ad9c30
Tsunemoto <cintia.tsunemoto@fatec.sp.gov.br>
2015-11-25 17:55:18 -0200

    Medicamento

--- MariosPet/MariosPet/Telas/FrmRemedio.cs
+++ MariosPet/MariosPet/Telas/FrmRemedio.cs
@@ -15,9 +15,15 @@ namespace MariosPet.Telas
     public partial class FrmRemedio : Form
     {
         Medicamento classeMed = new Medicamento();
+        CrudMedicamento CrudMed = new CrudMedicamento();
+
         public FrmRemedio()
         {
             InitializeComponent();
+            if (Estatica.id != 0)
+            {
+                CopiarParaFormulario();
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -46,13 +52,31 @@ namespace MariosPet.Telas
             classeMed.maximoEmEstoque = Convert.ToInt32(nudMaximo.Text);
         }
 
+        public void CopiarParaFormulario()
+        {
+            string sql = "select * from medicamento where id_medicamento = " + Estatica.id.ToString();
+            DataTable medicamento = CrudMed.consultaMedicamento(sql);
+
+            //Dados Medicamento
+            classeMed.id = Convert.ToInt32(medicamento.Rows[0][0].ToString());
+            txtMedicamento.Text = medicamento.Rows[0][1].ToString();
+            nudQuantidade.Text = Convert.ToString(medicamento.Rows[0][2].ToString());
+            nudMinimo.Text = Convert.ToString(medicamento.Rows[0][3].ToString());
+            nudMaximo.Text = Convert.ToString(medicamento.Rows[0][4].ToString());
+        }
         private void btnSalvar_Click_1(object sender, EventArgs e)
         {
             CopiarParaClasseMedicamento();
 
-            CrudMedicamento CrudMed = new CrudMedicamento();
-
-            CrudMed.inserirMedicamento(classeMed);
+            if (Estatica.id != 0)
+            {
+                CrudMed.alteraMedicamento(classeMed);
+                Estatica.id = 0;
+            }
+            else
+            {
+                CrudMed.inserirMedicamento(classeMed);
+            }            
         }
     }
 }
