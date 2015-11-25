5fda25e106a363c46a899b598f47363394ea046d
Tsunemoto <cintia.tsunemoto@fatec.sp.gov.br>
2015-11-25 20:10:01 -0200

    Servico

--- MariosPet/MariosPet/Telas/frmServico.cs
+++ MariosPet/MariosPet/Telas/frmServico.cs
@@ -15,9 +15,16 @@ namespace MariosPet.Telas
     public partial class FrmServico : Form
     {
         Servico classeServ = new Servico();
+        CrudServico CrudServ = new CrudServico();
+
         public FrmServico()
         {
             InitializeComponent();
+
+            if (Estatica.id != 0)
+            {
+                CopiarParaFormulario();
+            }
         }
 
         private void btnLimpar_Click(object sender, EventArgs e)
@@ -32,13 +39,20 @@ namespace MariosPet.Telas
             classeServ.preco = Convert.ToDouble(mstPreco.Text);
         }
 
+        public void CopiarParaFormulario()
+        {
+            string sql = "select * from servico where id_servico = " + Estatica.id.ToString();
+            DataTable servico = CrudServ.consultaServico(sql);
+
+            //Dados Serviço
+            classeServ.id = Convert.ToInt32(servico.Rows[0][0].ToString());
+            txtServico.Text = servico.Rows[0][1].ToString();
+            mstPreco.Text = Convert.ToString(servico.Rows[0][2].ToString());
+        }
+
         private void btnSalvar_Click(object sender, EventArgs e)
         {
-            CopiarParaClasseServico();
 
-            CrudServico CrudServ = new CrudServico();
-
-            CrudServ.inserirServico(classeServ);
         }
 
         private void btnVoltar_Click(object sender, EventArgs e)
@@ -51,10 +65,17 @@ namespace MariosPet.Telas
         private void btnSalvar_Click_1(object sender, EventArgs e)
         {
             CopiarParaClasseServico();
-
             CrudServico CrudServ = new CrudServico();
 
-            CrudServ.inserirServico(classeServ);
+            if (Estatica.id != 0)
+            {
+                CrudServ.alteraServico(classeServ);
+                Estatica.id = 0;
+            }
+            else
+            {
+                CrudServ.inserirServico(classeServ);
+            }            
         }
     }
 }
