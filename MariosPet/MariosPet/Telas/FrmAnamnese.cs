a7a4f3330fc1de1f6ffd4e8a7330cb8e9455daaf
Tsunemoto <cintia.tsunemoto@fatec.sp.gov.br>
2015-11-25 16:09:44 -0200

    Anamnese

--- MariosPet/MariosPet/Telas/FrmAnamnese.cs
+++ MariosPet/MariosPet/Telas/FrmAnamnese.cs
@@ -20,6 +20,11 @@ namespace MariosPet.Telas
         public FrmAnamnese()
         {
             InitializeComponent();
+
+            if (Estatica.id != 0)
+            {
+                CopiarParaFormulario();
+            }
         }
 
         private void btmLimpar_Click(object sender, EventArgs e)
@@ -65,13 +70,46 @@ namespace MariosPet.Telas
             classeAnam.data = DateTime.Now;
         }
 
+        public void CopiarParaFormulario()
+        {
+            CrudAnamnese CrudAnam = new CrudAnamnese();
+
+            string sql = "select * from anamnese id_anamnese = " + Estatica.id.ToString();
+            DataTable anamnese = CrudAnam.consultaAnamnese(sql);
+
+            //Anamnese            
+            classeAnam.idAnimal = Convert.ToInt32(anamnese.Rows[0][0].ToString());
+            classeAnam.idVeterinario = Convert.ToInt32(anamnese.Rows[0][1].ToString());
+            radioButtonSim.Text = anamnese.Rows[0][2].ToString();
+            radioButtonNao.Text = anamnese.Rows[0][2].ToString();
+            richTextBoxDoenca.Text = anamnese.Rows[0][3].ToString();
+            richTextBoxTratamento.Text = anamnese.Rows[0][4].ToString();
+            richTextBoxDigestorio.Text = anamnese.Rows[0][5].ToString();
+            richTextBoxCardioRespiratorio.Text = anamnese.Rows[0][6].ToString();
+            richTextBoxGenitoUrinario.Text = anamnese.Rows[0][7].ToString();
+            richTextBoxNervosoLocomotor.Text = anamnese.Rows[0][8].ToString();
+            richTextBoxPelesAnexos.Text = anamnese.Rows[0][9].ToString();
+            richTextBoxManejo.Text = anamnese.Rows[0][10].ToString();
+            classeAnam.data = Convert.ToDateTime(anamnese.Rows[0][11].ToString());
+            classeAnam.id = Convert.ToInt32(anamnese.Rows[0][12].ToString());
+        }
+
         private void btmSalvar_Click(object sender, EventArgs e)
         {
             CopiarParaClasseAnamnese();
 
             CrudAnamnese CrudAnam = new CrudAnamnese();
 
-            CrudAnam.inserirAnamnese(classeAnam);
+            if (Estatica.id != 0)
+            {
+                CrudAnam.alteraAnamnese(classeAnam);
+                Estatica.id = 0;
+            }
+            else
+            {
+                CrudAnam.inserirAnamnese(classeAnam);
+                classeAnam.id = Convert.ToInt32(CrudAnam.consultaAnamnese("Select top 1 ID_ANAMNESE from ANAMNESE order by ID_ANAMNESE desc").Rows[0][0].ToString());
+            }           
         }
 
         private void radioButtonSim_CheckedChanged(object sender, EventArgs e)
@@ -90,11 +128,11 @@ namespace MariosPet.Telas
         {
             if (radioButtonNao.Checked == true)
             {
-                radioButtonNao.Checked = false;
+                radioButtonSim.Checked = false;
             }
             else
             {
-                radioButtonNao.Checked = true;
+                radioButtonSim.Checked = true;
             }
         }
 
