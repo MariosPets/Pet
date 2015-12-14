732263183cee3c34af2c925f2119398e9f0531f2
leonardofernando06 <leonardo.sales@fatec.sp.gov.br>
2015-12-14 15:35:30 -0200

    teste

--- MariosPet/MariosPet/Classes/Vermifugo.cs
+++ MariosPet/MariosPet/Classes/Vermifugo.cs
@@ -8,8 +8,8 @@ namespace MariosPet.Classes
 {
     class Vermifugo
     {
-        public int id { get; set; }
-        public int idAnimal { get; set; }
+        public int idVermifugo { get; set; }
+        public int idFichaClinica { get; set; }
         public int idMedicamento { get; set; }
         public DateTime dataInicio { get; set; }
         public DateTime dataFim { get; set; }
