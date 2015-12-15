d820886957a48180eebd6ef2af8729f8f7c9aac5
leonardofernando06 <leonardo.sales@fatec.sp.gov.br>
2015-12-15 15:38:53 -0200

    sdf

---
+++ MariosPet/MariosPet/Crud/CrudVendas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MariosPet.Crud
+{
+    class CrudVendas
+    {
+        public int teste { get; set; }
+
+
+
+
+
+    }
+}
