a36e9e595b2dd37b5fa3242800f97d41fbea65ab
leonardofernando06 <leonardo.sales@fatec.sp.gov.br>
2015-12-15 15:36:43 -0200

    asd

---
+++ MariosPet/MariosPet/Classes/EntradaProduto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MariosPet.Classes
+{
+    class EntradaProduto
+    {
+        public int idEntradaProduto { get; set; }
+        public int idFornecedor { get; set; }
+        public string data { get; set; }
+        public string forma_pagamento { get; set; }
+        public double desconto { get; set; }
+        public double total { get; set; }
+
+
+        public int teste { get; set; }
+    }
+}
